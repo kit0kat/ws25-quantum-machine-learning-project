93193b3e7116cbae05fed46ae160cf7979c7156a
Nico Hofmann <nico.hofmann.2000@gmail.com>
2025-12-25 18:53:20 +0100

    Refactor: Custom observables as functions instead of classes

--- observables/summed_pairs.py
+++ observables/summed_pairs.py
@@ -4,23 +4,17 @@ import random
 from squlearn.observables import CustomObservable
 
 
-class SummedPairs(CustomObservable):
-
-    def __init__(self, num_qubits: int, bases: list[str], seed: int = 0):
-        super().__init__(
-            num_qubits=num_qubits,
-            operator_string=self._operator_string(num_qubits, bases, seed),
-            parameterized=True,
-        )
-
-    @staticmethod
-    def _operator_string(num_qubits: int, bases: list[str], seed: int = 0) -> list[str]:
-        random.seed(seed)
-        operator_string = []
-        for idx1, idx2 in itertools.combinations(range(num_qubits), 2):
-            op_list = list("I" * num_qubits)
-            op_list[idx1] = random.choice(bases)
-            op_list[idx2] = random.choice(bases)
-            operator_string.append("".join(op_list))
-        operator_string.append("I" * num_qubits)
-        return operator_string
+def SummedPairs(num_qubits: int, bases: list[str], seed: int = 0) -> CustomObservable:
+    random.seed(seed)
+    operator_string = []
+    for idx1, idx2 in itertools.combinations(range(num_qubits), 2):
+        op_list = list("I" * num_qubits)
+        op_list[idx1] = random.choice(bases)
+        op_list[idx2] = random.choice(bases)
+        operator_string.append("".join(op_list))
+    operator_string.append("I" * num_qubits)
+    return CustomObservable(
+        num_qubits=num_qubits,
+        operator_string=operator_string,
+        parameterized=True,
+    )
